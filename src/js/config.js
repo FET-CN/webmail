9d079934a1deaaf8a7797b9a41795528e21cf893
Kai Nordherd <262930583+kainordherd@users.noreply.github.com>
2026-08-19 02:07:27 +1000

    Config correction

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 window.config = {
     // Change this to false. It activates features specific to official mailecho.
-    official: true,
+    official: false,
     // The EHLO domain is sent during SMTP and sometimes shows up in "Received" headers
     ehlo: 'mailecho',
     // Set as the default "User-Agent" header in outgoing mail
@@ -12,9 +12,10 @@ window.config = {
     // Log level to output to developer console. -1 to disable
     log_level_console: WARN,
     // Raw protocol sockets are exposed under the versioned backend prefix.
-    imap_server: (window.location.protocol === "http:" ? "ws://" : "wss://") + window.location.host + "/v1/imap",
-    smtp_server: (window.location.protocol === "http:" ? "ws://" : "wss://") + window.location.host + "/v1/smtp",
-    events_server: (window.location.protocol === "http:" ? "ws://" : "wss://") + window.location.host + "/v1/events"
-    //imap_server: 'wss://example.com/path/to/imap',
-    //smtp_server: 'wss://example.com/path/to/smtp'
+    //imap_server: (window.location.protocol === "http:" ? "ws://" : "wss://") + window.location.host + "/v1/imap",
+    //smtp_server: (window.location.protocol === "http:" ? "ws://" : "wss://") + window.location.host + "/v1/smtp",
+    //events_server: (window.location.protocol === "http:" ? "ws://" : "wss://") + window.location.host + "/v1/events"
+    imap_server: 'wss://webmailapi.flowecho.org/v1/imap',
+    smtp_server: 'wss://webmailapi.flowecho.org/v1/smtp',
+    events_server: 'wss://webmailapi.flowecho.org/v1/events'
 };
